e9839307f97fb2fe88841492547e27e8c5c1875f
Jordan <skorlir@gmail.com>
2014-02-23 20:15:56 -0600

    resolve lurking conflict

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,5 @@
  */
 
 exports.index = function(req, res){
-<<<<<<< HEAD
   res.render('index', { title: 'Epic Database' });
-=======
-  res.render('index', { title: 'Express' });
->>>>>>> aec70257ba0899d9d85e6187978bcc9db27c4e97
 };
