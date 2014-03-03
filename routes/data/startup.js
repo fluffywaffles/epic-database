b7ce8b43b01c3e45ac022013bd0eb3cb19bbac42
Ben Rafshoon <benraf@u.northwestern.edu>
2014-03-03 12:26:01 -0600

    Cleaned up startup backend code to use promises, its much cleaner now

--- routes/data/startup.js
+++ routes/data/startup.js
@@ -6,34 +6,24 @@ module.exports = function(app) {
 		return query.populate("founders stage industries");
 	};
 	
-	
 	app.get("/data/startup", function(request, response) {
 		response.type("json");
 		var Startup = mongoose.model("Startup");
 		populateStartup(Startup.find({}))
-			.exec(function(err, startups) {
-				if(err) {
-					console.log(err);
-					response.send(500, "Error retrieving startups from DB");
-				}
-				response.send(startups);	
-				
-			});
+		.exec()
+		.then(function(startups) {
+			response.send(startups);
+		}, function(error) {
+			console.log(error);
+			response.send(500, "Error retrieving startups from DB");
+		});
 	});
 	
-	var addOrEditStartupCallback = function(query, response) {
-		return populateStartup(query).exec(function(err, updated) {
-			if(err) {
-				console.log(err);
-				response.send(400, "Error updating database");
-			} else {
-				console.log(updated);
-				response.send(updated, 200);
-			}
-		});
-	};
-	
-	var processIndustries = function(startup, onDone) {
+	var processIndustries = function(startup) {
+		console.log("Process industries");
+		var promise = new mongoose.Promise();
+		var Industry = mongoose.model("Industry");
+		
 		if(startup.industries && startup.industries.length > 0) {
 			var count = 0;
 			var industryIdMap = [];
@@ -41,7 +31,7 @@ module.exports = function(app) {
 				count++;
 				if(count == startup.industries.length) {
 					startup.industries = industryIdMap;
-					onDone(undefined);
+					promise.fulfill(startup);
 				}
 			};
 			
@@ -52,8 +42,8 @@ module.exports = function(app) {
 				} else {
 					Industry.create(industry, function(error, newIndustry) {
 						if(error) {
-							console.log("Error with new industry");
-							onDone("Error in industry");
+							console.log(error);
+							promise.reject("Error adding new industry");
 							//Probably should undo or switch to multi/upsert transaction
 						} else {
 							industryIdMap[index] = newIndustry._id;
@@ -63,11 +53,16 @@ module.exports = function(app) {
 				}
 			});
 		} else {
-			onDone(undefined);
+			promise.fulfill(startup);
 		}
+		return promise;
 	};
 	
-	var processFounders = function(startup, onDone) {		
+	var processFounders = function(startup) {
+		console.log("Process founders");
+		var promise = new mongoose.Promise();
+		var Person = mongoose.model("Person");
+		
 		if(startup.founders && startup.founders.length > 0) {
 			var founderIdMap = [];
 			var count = 0;
@@ -75,7 +70,7 @@ module.exports = function(app) {
 				count++;
 				if(count == startup.founders.length) {
 					startup.founders = founderIdMap;
-					onDone(undefined);
+					promise.fulfill(startup);
 				}
 			};
 			startup.founders.forEach(function(founder, index) {
@@ -85,8 +80,8 @@ module.exports = function(app) {
 				} else {
 					Person.create(founder, function(error, newPerson) {
 						if(error) {
-							console.log("Error with new person");
-							response.send(404);
+							console.log(error);
+							promise.reject("Error adding founder");
 						} else {
 							founderIdMap[index] = newPerson._id;
 							oneDone();
@@ -95,61 +90,45 @@ module.exports = function(app) {
 				}
 			});
 		} else {
-			onDone(undefined);
+			promise.fulfill(startup);
 		}
+		return promise;
+		
+	};
+	
+	var errorHandler = function(error) {
+		console.log(error);
+		response.send(500, error);
 	};
 	
 	//THIS MUST COME BEFORE THE EDIT PATH!!
 	app.post("/data/startup/new", function(request, response) {
 		var toAdd = request.body;
-		console.log(toAdd);
 		var Startup = mongoose.model("Startup");
-		processFounders(toAdd, function(error) {
-			if(error) {
-				console.log(error);
-				response.send(500, "Error saving to DB");
-			}
-			processIndustries(toAdd, function(error) {
-				if(error) {
-					console.log(error);
-					response.send(500, "Error saving to DB");
-				}
-				Startup.create(toAdd, function(err, added) {
-					if(err) {
-						console.log(err);
-						response.send(500, "Error saving to DB");
-					} else {
-						addOrEditStartupCallback(Startup.findById(added._id), response);
-					}
-				});
-				
-			});
-		});
 		
-		
+		processFounders(toAdd)
+		.then(processIndustries)
+		.then(function(processed) { return Startup.create(processed); })
+		.then(function(created) { return Startup.findById(created._id); })
+		.then(populateStartup)
+		.then(function(query) { return query.exec(); })
+		.then(function(startup) { response.send(startup); },
+			errorHandler);
 	});
-	
 	
 	app.post("/data/startup/:id", function(request, response) {
 		var toUpdate = request.body;
 		delete toUpdate._id;
 		var id = request.params.id;
 		var Startup = mongoose.model("Startup");
-		var Person = mongoose.model("Person");
-		var Industry = mongoose.model("Industry");
-		processFounders(toUpdate, function(error) {
-			if(error) {
-				console.log(error);
-				response.send(500, "Error saving to DB");
-			}
-			processIndustries(toUpdate, function(error) {
-				if(error) {
-					console.log(error);
-					response.send(500, "Error saving to DB");
-				}
-				addOrEditStartupCallback(Startup.findByIdAndUpdate(id, toUpdate), response);
-			});
-		});
+		
+		processFounders(toUpdate)
+		.then(processIndustries)
+		.then(function(processed) { return Startup.findByIdAndUpdate(id, processed); })
+		.then(populateStartup)
+		.then(function(query) { return query.exec(); })
+		.then(function(startup) { response.send(startup); },
+			errorHandler);
 	});
 	
 	app.del("/data/startup/:id", function(request, response) {
@@ -157,22 +136,13 @@ module.exports = function(app) {
 		var id = new mongoose.Types.ObjectId(request.params.id);
 		var Startup = mongoose.model("Startup");
 		console.log(id);
-		Startup.remove({_id: id}, function(error) {
-			if(error) {
-				response.send("Error deleting", 404);
-			} else {
-				//Remove from any startups that have this as a related startup
-				Startup.update({relatedStartups: id}, {$pull: {relatedStartups: id}}, {multi: true}, function(error, numAffected, rawResponse) {
-					if(error) {
-						console.log(error);
-						response.send("Error deleting", 404);
-					}
-					console.log(numAffected);
-					response.send(200);	
+		Startup.update({relatedStartups: id}, {$pull: {relatedStartups: id}}, {multi: true}).exec()
+			.then(function() { return Startup.remove({_id: id}).exec(); })
+			.then(function() { response.send(200); },
+				function(error) {
+					console.log(error);
+					response.send("Error deleting", 404);
 				});
-			}
-		});
-		
 	});
 };
 	
